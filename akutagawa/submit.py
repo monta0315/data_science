ea1e99e5115a987841548abff70f2fd7db45c16c
monta0315 <monta0315@docomo.ne.jp>
2021-03-28 18:01:40 +0900

    kernel1の途中

--- akutagawa/submit.py
+++ akutagawa/submit.py
@@ -1,4 +1,12 @@
 import numpy as np
 import pandas as pd
 
-csv_input=pd.read_csv()
+test=pd.read_csv("data/test.csv")
+train=pd.read_csv("data/train.csv")
+
+def main():
+  data = pd.concat([train,test])
+  data.isnull().sum()
+
+
+main()
